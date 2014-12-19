5e1068e7ce649e92063bbf9f35a5421aa0b88c2a
Andy <andygnewman@gmail.com>
2014-12-19 16:08:14 +0000

    refactor using .to_proc to convert symbol into proc

--- lib/andyarray.rb
+++ lib/andyarray.rb
@@ -2,31 +2,16 @@ class Array
 
   def andy_inject(a = nil, b = nil, &block)
     a == nil || a.class == Symbol ? memo = self.first : memo = a
-    if block != nil && a == nil
-      self[1..self.length].each do | n |
+    if block != nil
+      a == nil ? range_start = 1 : range_start = 0
+      self[range_start..self.length].each do | n |
         memo = block.call(memo, n)
       end
-    elsif block != nil && a != nil
-      self[0..self.length].each do | n |
-        memo = block.call(memo, n)
-      end
-    elsif a.class == Symbol && b == nil
-      self[1..self.length].each do | n |
-        case a.slice(-1)
-        when '+' then memo = memo + n
-        when '-' then memo = memo - n
-        when '*' then memo = memo * n
-        else p 'unknown operator'
-        end
-      end
-    elsif b.class == Symbol && a != nil
-      self[0..self.length].each do | n |
-        case b.slice(-1)
-        when '+' then memo = memo + n
-        when '-' then memo = memo - n
-        when '*' then memo = memo * n
-        else p 'unknown operator'
-        end
+    elsif a.class == Symbol || b.class == Symbol
+      a.class == Symbol ? range_start = 1 : range_start = 0
+      a.class == Symbol ? my_proc = a.to_proc : my_proc = b.to_proc
+      self[range_start..self.length].each do | n |
+        memo = my_proc.call(memo, n)
       end
     else
       p 'incorrect syntax for andy_inject'
