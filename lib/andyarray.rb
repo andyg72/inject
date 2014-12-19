4cc628cef67446a3f21b14cb9fbac46b56bc3cca
Andy <andygnewman@gmail.com>
2014-12-19 16:22:39 +0000

    refactor set two variables in a ternary conditional

--- lib/andyarray.rb
+++ lib/andyarray.rb
@@ -5,12 +5,11 @@ class Array
       a == nil ? range_start = 1 : range_start = 0
       self[range_start..self.length].each { | n |  memo = block.call(memo, n) }
     elsif a.class == Symbol || b.class == Symbol
-      a.class == Symbol ? range_start = 1 : range_start = 0
-      a.class == Symbol ? my_proc = a.to_proc : my_proc = b.to_proc
+      range_start, my_proc = a.class == Symbol ? [1, a.to_proc] : [0, b.to_proc]
       self[range_start..self.length].each { | n | memo = my_proc.call(memo, n) }
     else
       p 'incorrect syntax for andy_inject'
     end
     return memo
-  end # method end
-end # class end
+  end
+end
