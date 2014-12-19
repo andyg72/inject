a3444298cecaca638b3c42262b5d248a65036924
Andy <andygnewman@gmail.com>
2014-12-19 16:16:53 +0000

    further refactoring

--- lib/andyarray.rb
+++ lib/andyarray.rb
@@ -1,18 +1,13 @@
 class Array
-
   def andy_inject(a = nil, b = nil, &block)
     a == nil || a.class == Symbol ? memo = self.first : memo = a
     if block != nil
       a == nil ? range_start = 1 : range_start = 0
-      self[range_start..self.length].each do | n |
-        memo = block.call(memo, n)
-      end
+      self[range_start..self.length].each { | n |  memo = block.call(memo, n) }
     elsif a.class == Symbol || b.class == Symbol
       a.class == Symbol ? range_start = 1 : range_start = 0
       a.class == Symbol ? my_proc = a.to_proc : my_proc = b.to_proc
-      self[range_start..self.length].each do | n |
-        memo = my_proc.call(memo, n)
-      end
+      self[range_start..self.length].each { | n | memo = my_proc.call(memo, n) }
     else
       p 'incorrect syntax for andy_inject'
     end
