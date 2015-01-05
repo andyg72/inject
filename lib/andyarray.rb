0f9544e142fe04d20de951c0601613d66b2f5925
Andy <andygnewman@gmail.com>
2015-01-05 14:53:39 +0000

    recursive method for rewrite of inject working

--- lib/andyarray.rb
+++ lib/andyarray.rb
@@ -6,21 +6,15 @@ class Array
     return memo
   end
 
-#  example of how recursion could work from coach Henry
-  # def henry_recur(sym, memo, sequence)
-  #   if sequence.first
-  #     new_memo = memo.send sym, memo, sequence.first
-  #     henry_recur sym, new_memo, sequence.shift
-  #   else
-  #     memo
-  #   end
-  # end
-
-  def andy_inject_recursion(a = nil, b = nil, &block)
-    #write a method to do the same, but using a recursive method
-    #key to it is to figure out how to iterate through the array to find n 
-    #whilst recursively generating the value for memo
-    #e.g. memo = n + method to get memo
+  def andy_inject_recursion(a = nil, b = nil, &my_proc)
+    memo, array = a == nil || a.class == Symbol ? [self.first, self[1..self.length]] : [a, self]
+    a.class == Symbol ? my_proc = a.to_proc : b.class == Symbol ? my_proc = b.to_proc : my_proc 
+    if array.first
+      new_memo = my_proc.call(memo, array.first)
+      array[1..array.length].andy_inject_recursion(new_memo, &my_proc)
+    else
+      memo
+    end
   end
 
 end
