2c9fb87905a87745f9c8d343b08138abe2db7f96
Andy <andygnewman@gmail.com>
2015-01-05 15:06:01 +0000

    tidy up way in which my_proc variable is set for readability

--- lib/andyarray.rb
+++ lib/andyarray.rb
@@ -1,14 +1,14 @@
 class Array
   def andy_inject(a = nil, b = nil, &my_proc)
     memo, array = a == nil || a.class == Symbol ? [self.first, self[1..self.length]] : [a, self]
-    a.class == Symbol ? my_proc = a.to_proc : b.class == Symbol ? my_proc = b.to_proc : my_proc  
+    my_proc = a.class == Symbol ? a.to_proc : b.class == Symbol ? b.to_proc : my_proc
     array.each { | n | memo = my_proc.call(memo, n) }
     return memo
   end
 
   def andy_inject_recursion(a = nil, b = nil, &my_proc)
     memo, array = a == nil || a.class == Symbol ? [self.first, self[1..self.length]] : [a, self]
-    a.class == Symbol ? my_proc = a.to_proc : b.class == Symbol ? my_proc = b.to_proc : my_proc 
+    my_proc = a.class == Symbol ? a.to_proc : b.class == Symbol ? b.to_proc : my_proc
     if array.first
       new_memo = my_proc.call(memo, array.first)
       array[1..array.length].andy_inject_recursion(new_memo, &my_proc)
